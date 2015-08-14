a1cccdd585cbd5995a845956f9e2460d85d1bec6
Myo Zar Ni <myo.zarni@thought.co.uk>
2015-08-14 17:20:35 +0100

    comment amends

--- Helpers/CommentResponseInfoExtensions.cs
+++ Helpers/CommentResponseInfoExtensions.cs
@@ -1,4 +1,10 @@
+using System.Linq;
+using MrCMS.Entities.Documents.Web;
+using MrCMS.Web.Apps.Commenting.Entities;
 using MrCMS.Web.Apps.Commenting.Models;
+using MrCMS.Web.Apps.Commenting.Services;
+using MrCMS.Website;
+using NHibernate;
 
 namespace MrCMS.Web.Apps.Commenting.Helpers
 {
@@ -7,6 +13,16 @@ namespace MrCMS.Web.Apps.Commenting.Helpers
         public static bool IsSuccess(this ICommentResponseInfo commentResponseInfo)
         {
             return commentResponseInfo != null && commentResponseInfo.Type == CommentResponseType.Success;
+        }
+
+        public static int GetCommentsCount(this Webpage webpage)
+        {
+            var commentInfo = MrCMSApplication.Get<ISession>().QueryOver<Comment>()
+                .Where(
+                    comment =>
+                        comment.Webpage == webpage && comment.Approved == true &&
+                        comment.InReplyTo == null).List();
+            return commentInfo.Count;
         }
     }
 }
